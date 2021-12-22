368a59757b304fecbed8e7ee456faa12cbd49617
Audrey <audcsm@gmail.com>
2021-12-22 02:15:32 +0000

    Modified development config to allow execution in port 8000

--- server/rails_api/config/environment.rb
+++ server/rails_api/config/environment.rb
@@ -1,5 +1,7 @@
 # Load the Rails application.
 require_relative "application"
 
+Rails.application.config.hosts << "8000-copper-slug-6fjpzrxa.ws-us23.gitpod.io"
+
 # Initialize the Rails application.
 Rails.application.initialize!
--- server/rails_api/config/environments/development.rb
+++ server/rails_api/config/environments/development.rb
@@ -2,8 +2,7 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.hosts << "3000-copper-slug-6fjpzrxa.ws-eu17.gitpod.io"
-  config.hosts << "8000-copper-slug-6fjpzrxa.ws-eu21.gitpod.io"
+  config.hosts << "8000-copper-slug-6fjpzrxa.ws-us23.gitpod.io"
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
