3b0919d132defa88d0cae06f03ada49f6af67498
Audrey <audcsm@gmail.com>
2022-01-13 20:52:45 +0000

    Vue client running, docker/make does not work at the moment. Hold for later commits

---
+++ client/vue_client/src/components/NavBar.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <v-toolbar dark>
+        <v-toolbar-title
+          @click="redirectNewItem">New Item
+        </v-toolbar-title>
+
+      <div class="flex-grow-1"></div>
+      <v-toolbar-items>
+        <v-btn text>New Item</v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    redirectNewItem () {
+      this.$router.push('/')
+    }
+  }
+}
+</script>
---
+++ client/vue_client/src/views/Home.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="home">
+    <h1>FreeCycle Client</h1>
+    <v-container>
+      <v-row>
+        <v-col>
+          <v-card>
+            <v-toolbar>
+              <v-toolbar-title>Enter new item</v-toolbar-title>
+              <div></div>
+            </v-toolbar>
+            <v-card-text>
+              <v-form>
+                <v-text-field label="User ID" name="user_id" type="text">g</v-text-field>
+                <v-text-field label="Keywords" name="keywords" type="text"></v-text-field>
+                <v-text-field label="Description" name="desc" type="text"></v-text-field>
+                <v-text-field label="Image" name="imglink" type="text"></v-text-field>
+                <v-text-field label="Latitude" name="lat" type="int"></v-text-field>
+                <v-text-field label="Longitude" name="lon" type="int"></v-text-field>
+              </v-form>
+            </v-card-text>
+            <v-card-actions>
+              <div></div>
+              <v-btn>Submit</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
